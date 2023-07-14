163dd905de1069bb6460ae8f8cce622884b9af3c
Rajin Sakha <rajinsakha07@gmail.com>
2023-07-15 00:17:16 +0545

    productDescription 75% done

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const sideMenu = document.querySelector('.side-menu');
 const categoryLinks = [...document.getElementsByClassName("category-link")]
 const productList = [...document.getElementsByClassName("explore-products")]
 
+const imageList = [...document.getElementsByClassName("image-link")];
+const mainImages = [...document.getElementsByClassName("main-image-link")];
+
 function openMenu(){
     sideMenu.style.left = "0";
 }
@@ -17,11 +20,9 @@ categoryLinks.forEach((list) => {
             if(list.textContent.toLowerCase() === product.id){
                 product.classList.add('active-tab');
                 list.classList.add('active-link');
-                console.log(list.classList);
                 
-
                 categoryLinks.forEach((list) =>{
-                    if(list.textContent.toLowerCase !== product.id){
+                    if(list.textContent.toLowerCase() !== product.id){
                         list.classList.remove('active-link');
                     } 
                 })
@@ -34,3 +35,25 @@ categoryLinks.forEach((list) => {
     })
     
 });
+
+// For Product Description page to display each image
+imageList.forEach((image)=>{
+    image.addEventListener('click',()=>{
+        mainImages.forEach((mainImage)=>{
+            if(image.src === mainImage.src){
+                mainImage.classList.add('active-main-image');
+                image.classList.add('active-image');
+
+                imageList.forEach((image)=>{
+                    if(image.src !== mainImage.src){
+                        image.classList.remove('active-image');
+                    }
+                })
+            }
+            else{
+                mainImage.classList.remove('active-main-image');
+            }
+        })
+    })
+
+})
